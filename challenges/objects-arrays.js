cda2dd6a6c145aa0952f254357975ae8ab3122ff
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-01-24 22:11:32 -0500

    have all but adavanced arrays 2 and 4 working

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -166,16 +166,24 @@ The zoos are concerned about animals with a lower population count. Using filter
 create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter((zooAnimals) => {
+  return zooAnimals.population < 5;
+});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback 
+(which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+
+const populationTotal = zooAnimals.reduce((total, zooAnimals) => {
+  return total += zooAnimals.population;
+})
+console.log('this is' + populationTotal);
 
 
 /*
